9ea3ce57380ab7f8861413b7834aefa181ae4d95
Oğuzhan Mangır <oguzhan.mangir55@gmail.com>
2018-07-12 13:09:26 +0300

    imports updated

--- IndoorPositioningSystem/Forms/SVC.py
+++ IndoorPositioningSystem/Forms/SVC.py
@@ -1,15 +1,7 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'SVC.ui'
-#
-# Created by: PyQt5 UI code generator 5.9.2
-#
-# WARNING! All changes made in this file will be lost!
-
 from PyQt5 import QtCore, QtGui, QtWidgets
 from sklearn.svm import SVC
 
-from VTYS_Project_Final.Source.use_algorithms import AlgorithmOperation
+from Source.use_algorithms import AlgorithmOperation
 
 
 class Ui_FormSetParamSVC(object):
